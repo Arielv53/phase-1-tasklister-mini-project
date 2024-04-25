3ad70d9c29fcd1c5e745dd20c235a4602e4d3dfa
ariel vargas <arielvs53@gmail.com>
2024-04-25 15:56:05 -0400

    task lists

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.querySelector("#create-task-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskName = event.target['new-task-description'].value;
+    
+    const newItem = document.createElement("li");
+    newItem.textContent = taskName;
+
+    const unorderedList = document,querySelector("#tasks");
+    unorderedList.append(newItem);
+  })
 });
